972d2b6e4b15fa13ad527b75642b7b4778af5df0
unknown <chethankrishan116@gmail.com>
2023-10-08 20:39:17 -0700

    contact

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// sliding bar
+
 const bar=document.getElementById("bar");
 const navbar=document.getElementById("navbar");
 const close=document.getElementById("close");
@@ -9,3 +11,29 @@ bar.addEventListener('click',() => {
 close.addEventListener('click',() => {
     navbar.classList.remove('active')
 })
+
+
+// singlepro
+
+    var mainImg=document.getElementById("mainImg")
+    var smallImg=document.getElementsByClassName("small-img1")
+
+    smallImg[0].onclick= function()
+    {
+        mainImg.src = smallImg[0].src;
+    }
+
+    smallImg[1].onclick= function()
+    {
+        mainImg.src = smallImg[1].src;
+    }
+
+    smallImg[2].onclick= function()
+    {
+        mainImg.src = smallImg[2].src;
+    }
+
+    smallImg[3].onclick= function()
+    {
+        mainImg.src = smallImg[3].src;
+    }
